a98d099fef51125fe5db3defd3ba484bfb9e8652
QT8485 <quet663@gmail.com>
2025-02-05 16:41:06 +0530

    frigate demo

---
+++ assets/js/blog.js
@@ -0,0 +1,61 @@
+// category
+$('.rs-categories-slider').owlCarousel({
+    loop:true,
+    margin:40,
+    dots: true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+// category end
+
+// latest post
+$('.latest-posts').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+// latest post
+
+
+// drive pick
+$('.rs-drive-pick').owlCarousel({
+    loop:true,
+    margin:40,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+// drive pick end
